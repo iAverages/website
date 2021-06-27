df9e31bfb7d21a5e2b379e5e475abed2ed1c1438
iAvearges <31937542+iAverages@users.noreply.github.com>
2021-06-27 13:46:14 +0100

    Made footer always at the bottom of the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,21 @@ import Home from "./Home";
 import Error from "./Error";
 import About from "./About";
 import Footer from "../components/Footer";
-import "../styles/global.module.css";
+import "../styles/global.css";
 
 function Main() {
     return (
         <>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/about" component={About} />
-                <Route exact path="/contact" component={Home} />
-                <Route exact path="/project" component={Home} />
-                <Route path="*" component={() => <Error error="404 Not found" />} />
-            </Switch>
+            <div className={"wrapper"}>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contact" component={Home} />
+                    <Route exact path="/project" component={Home} />
+                    <Route path="*" component={() => <Error error="404 Not found" />} />
+                </Switch>
+            </div>
+
             <Footer />
         </>
     );

4283a3b3a92753f4e21613d5f5f440fc4bca2fc4
iAvearges <31937542+iAverages@users.noreply.github.com>
2021-06-27 11:58:51 +0100

    Finished footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,73 @@
 import React from "react";
 import styles from "../styles/footer.module.css";
 import Link from "./Link";
+import { version } from "../../package.json";
+import { useMediaQuery } from "react-responsive";
 
 const Footer: React.FC = () => {
+    const swapOrder = useMediaQuery({ query: "(max-width: 601px)" });
+
+    const Left = () => (
+        <div id={styles.left} className={styles.column}>
+            <h3 className={styles.header}>External</h3>
+            <div className={styles.links}>
+                <Link href={"https://secure-heaven.com/"} label={"Secure Heaven"} newTab />
+                <Link href={"https://github.com/iAverages"} label={"Github"} newTab />
+                <Link href={"https://paste.danielraybone.com/"} label={"Hastebin"} newTab />
+                <Link href={"https://status.danielraybone.com/"} label={"Status"} newTab />
+                <Link href={"https://status.danielraybone.com/"} label={"Discord"} newTab />
+            </div>
+        </div>
+    );
+
+    const Center = () => (
+        <div id={styles.center} className={styles.column}>
+            <img className={styles.logo} height={200} src={"/logo.png"} alt={"Logo"} />
+            <p className={styles.copyright}>
+                ©️ {new Date().getFullYear()} Daniel Raybone - version <span>{version}</span>
+            </p>
+        </div>
+    );
+
+    const Right = () => (
+        <div id={styles.right} className={styles.column}>
+            <h3 className={styles.header}>Public Projects</h3>
+            <div className={styles.links}>
+                <Link href={"https://github.com/iAverages/Yin"} label={"Yin Bot"} newTab />
+                <Link
+                    href={"https://github.com/iAverages/ImageUploadAPI"}
+                    label={"Image Upload API"}
+                    newTab
+                />
+                <Link
+                    href={"https://github.com/iAverages/SkinsAPI"}
+                    label={"Minecraft Skins API"}
+                    newTab
+                />
+                <Link
+                    href={"https://github.com/iAverages/website"}
+                    label={"Website Source"}
+                    newTab
+                />
+            </div>
+        </div>
+    );
+
     return (
         <div className={styles.wrapper}>
-            {/* <div className={styles.left}>
-                <h3>Pages</h3>
-                <Link href={"/about"} label={"Testing"} />
-                <Link href={"/projects"} label={"Projects"} />
-                <Link href={"/contact"} label={"Contact"} />
-            </div> */}
-            <div className={styles.center}>
-                <img height={200} src={"/logo.png"} alt={"Logo"} />
-                <p>©️ {new Date().getFullYear()} Daniel Raybone</p>
-            </div>
-            <div className={styles.right}>
-                <h3>External</h3>
-                <div className={styles.links}>
-                    <Link href={"https://secure-heaven.com/"} label={"Secure Heaven"} newTab />
-                    <Link href={"https://github.com/iAverages"} label={"Github"} newTab />
-                    <Link href={"https://paste.danielraybone.com/"} label={"Hastebin"} newTab />
-                    <Link href={"https://status.danielraybone.com/"} label={"Status"} newTab />
-                </div>
-            </div>
+            {swapOrder ? (
+                <>
+                    <Left />
+                    <Right />
+                    <Center />
+                </>
+            ) : (
+                <>
+                    <Left />
+                    <Center />
+                    <Right />
+                </>
+            )}
         </div>
     );
 };
